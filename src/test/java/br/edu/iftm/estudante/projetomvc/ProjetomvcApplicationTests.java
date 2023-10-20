295a46f549fc4ee0a674101435bd85ab40f66036
Maria Eduarda <mariaeduarda050501@gmail.com>
2023-10-20 16:53:51 -0300

    Projeto Criado

---
+++ src/test/java/br/edu/iftm/estudante/projetomvc/ProjetomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.estudante.projetomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
