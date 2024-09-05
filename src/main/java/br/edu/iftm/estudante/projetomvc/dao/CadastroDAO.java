edd55c626074c6440da5e331a39f85cac5afcb2b
Maria Eduarda <mariaeduarda050501@gmail.com>
2024-09-04 23:56:25 -0300

    ajuste-sonar: (java:S1128)Unnecessary imports should be removed

--- src/main/java/br/edu/iftm/estudante/projetomvc/dao/CadastroDAO.java
+++ src/main/java/br/edu/iftm/estudante/projetomvc/dao/CadastroDAO.java
@@ -1,6 +1,5 @@
 package br.edu.iftm.estudante.projetomvc.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
